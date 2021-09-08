67ae13294180566451b6f32a2271667c28264701
zengwu <57541193+zngw@users.noreply.github.com>
2021-09-08 18:49:18 +0800

    Update log.go
    日志改用beego日志模块

--- log.go
+++ log.go
@@ -1,106 +1,137 @@
-/**
-* File:log.go
-* Copyright: Copyright (c) 2019
-* Created on 2019-11-11
-* Author:zengwu
-* Version 1.0
-* Title: 日志类
- */
+// @Title 日志封装
+// @Description $
+// @Author  55
+// @Date  2021/9/7
 package log
 
 import (
 	"fmt"
-	"github.com/cihub/seelog"
-	"runtime"
-	"strings"
+
+	"github.com/beego/beego/v2/core/logs"
 )
 
-// 输入日志的标签
-var showTag map[string]bool
-var logFolder string
+// 日志对象
+var Log *logs.BeeLogger
 
-// 初始化日志
-// file 日志文件名 ""为logs/log.info.2016-02-01;tags 显示的tag, nil为不设置显示tags
-func Init(file string, tags []string) (err error) {
-	// 配置日志文件，运行文件所在目录/logs/文件名
-	if len(file) > 0 {
-		fileConfig = strings.Replace(fileConfig, "./logs/log", file, -1)
-	}
+// 显示tags对象
+var Tags = make(map[string]bool)
 
-	logger, _ := seelog.LoggerFromConfigAsString(fileConfig)
-	err = seelog.ReplaceLogger(logger)
-	if err != nil {
-		fmt.Println("log init error.", err)
-		return 
-	}
+// 默认初始化
+func init() {
+	Log = logs.NewLogger(200)
 
-	// 根据配置显示tag
-	if tags!=nil {
-		showTag = make(map[string]bool)
-		for _, tag := range tags {
-			showTag[tag] = true
-		}
-	}
+	// 日志输出调用的文件名和文件行号，默认为false
+	Log.EnableFuncCallDepth(true)
+
+	// 如果你的应用自己封装了调用 log 包,那么需要设置 SetLogFuncCallDepth
+	// 默认是 2,也就是直接调用的层级,如果你封装了多层,那么需要根据自己的需求进行调整.
+	Log.SetLogFuncCallDepth(3)
+}
+
+// 兼容接口
+func Init(logFile string, tags []string) (err error) {
+	setLogFile("file", logFile, 30, true)
+	setTags(tags)
 
 	return
 }
 
-// 重新设置显示标志
-func ReplaceShowTag(tags []string) {
-	if tags==nil {
-		showTag = nil
+// 初始化日志参数
+func InitLog(logWay string, logFile string, logLevel string, maxDays int64, disableLogColor bool, tags []string) {
+	setLogFile(logWay, logFile, maxDays, disableLogColor)
+	setLogLevel(logLevel)
+	setTags(tags)
+}
+
+// 设置显示tag
+func setTags(tags []string)  {
+	if tags != nil {
+		for _, tag := range tags {
+			Tags[tag] = true
+		}
+	}
+}
+
+// 设置日志文件参数
+// logWay: file or console
+func setLogFile(logWay string, logFile string, maxdays int64, disableLogColor bool) {
+	if logWay == "console" {
+		params := ""
+		if disableLogColor {
+			params = fmt.Sprintf(`{"color": false}`)
+		}
+		_ = Log.SetLogger("console", params)
+	} else {
+		params := fmt.Sprintf(`{"filename": "%s", "maxdays": %d}`, logFile, maxdays)
+		_ = Log.SetLogger("file", params)
+	}
+}
+
+// 设置日志显示等级
+// value: error, warning, info, debug, trace
+func setLogLevel(logLevel string) {
+	level := 4 // warning
+	switch logLevel {
+	case "error":
+		level = 3
+	case "warn":
+		level = 4
+	case "info":
+		level = 6
+	case "debug":
+		level = 7
+	case "trace":
+		level = 8
+	default:
+		level = 4
+	}
+	Log.SetLevel(level)
+}
+
+// 获取tag是否显示
+func getTag(tag string) (msg string, show bool) {
+	if _, ok := Tags[tag]; !ok {
 		return
 	}
 
-	showTag = make(map[string]bool)
-	for _, tag := range tags {
-		showTag[tag] = true
+	msg = "[Tag:"+tag+"] "
+	show = true
+
+	return
+}
+
+// wrap log
+func Error(tag, format string, v ...interface{}) {
+	tag, show := getTag(tag)
+	if show {
+		Log.Error(tag+format, v...)
 	}
 }
 
-// 输出日志
-func Trace(tag string, v ...interface{}) {
-	if showTag != nil {
-		if _, ok := showTag[tag]; !ok {
-			return
-		}
+func Warn(tag,format string, v ...interface{}) {
+	tag, show := getTag(tag)
+	if show {
+		Log.Warn(tag+format, v...)
 	}
-
-	seelog.Trace("- [Tag:"+tag+"] ", v)
 }
 
-// 输出错误日志
-func Error(v ...interface{}) {
-	pc, filename, line, _ := runtime.Caller(1)
-	funcName := runtime.FuncForPC(pc).Name()
-	caller := fmt.Sprintf("- [%s:%d - %s] ", filename, line, funcName)
-	_ = seelog.Error(caller, v)
+func Info(tag,format string, v ...interface{}) {
+	tag, show := getTag(tag)
+	if show {
+		Log.Info(tag+format, v...)
+	}
 }
 
-var fileConfig = `
-<!-- type 设置记录器类型 https://github.com/cihub/seelog/wiki/Logger-types-referenceminlevel 设置日志最低级别; 
-maxlevel 设置日志的最高级别也可以通过 <seelog levels="trace,info,critical"> 设置日记级别 -->
-<seelog type="asynctimer" asyncinterval="5000000" minlevel="trace" maxlevel="error">
-	<!-- <outputs> formatid 指定日志输出的格式(格式在<formats>标签中定义) -->
-    <outputs formatid="trace">
-		<filter levels="trace,info,debug">
-			<!-- <console> 标签表示输出到终端 -->
-			<console formatid="colored" />
-			<!-- <rollingfile>滚动文件(定期清除过期日志) formatid: 指定日志格式; type="size/date" 按大小/按日期; maxsize: 单日志文件最大大小; maxrools: 最大文件数 -->
-            <rollingfile formatid="trace" type="date" filename="./logs/log.info" datepattern="2006-01-02" fullname="true" maxrolls="5"/>
-        </filter>
-        <filter levels="error,warn">
-			<console formatid="coloredErr" />
-            <rollingfile formatid="trace" type="date" filename="./logs/log.err" datepattern="2006-01-02" fullname="true" maxrolls="30"/>
-        </filter>
-    </outputs>
-   
-    <formats>
- 		<!-- <formats> 定制日志的输出格式https://github.com/cihub/seelog/wiki/Format-reference -->
-        <format id="trace" format="%Date(2006-01-02 15:04:05.000) [%LEVEL] %Msg%n" />
-        <format id="err" format="%Date(2006-01-02 15:04:05.000) [%LEVEL] [%File:%Line-%Func] %Msg%n" />
-        <format id="colored" format="%Date(2006-01-02 15:04:05.000) %EscM(42)[%LEVEL]%EscM(49) %Msg%n%EscM(0)" />
-		<format id="coloredErr" format="%Date(2006-01-02 15:04:05.000) %EscM(41)[%LEVEL]%EscM(49) [%File:%Line-%Func] %Msg%n%EscM(0)"/>
-    </formats>
-</seelog>
-`
+func Debug(tag,format string, v ...interface{}) {
+	tag, show := getTag(tag)
+	if show {
+		Log.Debug(tag+format, v...)
+	}
+}
+
+func Trace(tag,format string, v ...interface{}) {
+	tag, show := getTag(tag)
+	if show {
+		Log.Trace(tag+format, v...)
+	}
+}
